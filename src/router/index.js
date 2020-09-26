eb04dbe8a954d016a6710e0ff9313e1bc6912437
lavikara <karatemitayo@gmail.com>
2020-09-26 16:55:17 +0100

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="home">
+    <img class="logo" src="../assets/logo.png" alt="logo" />
+    <h1>👋Hello world🌎</h1>
+  </div>
+</template>
